1870aba7ca726a534aae4fa2e8ae77c028d1d829
Robert Soeldner <r.soeldner@gmail.com>
2022-08-09 13:12:33 +0200

    change injective rule to inclusion

--- Rule.thy
+++ Rule.thy
@@ -3,9 +3,19 @@ theory Rule
 begin
 
 locale rule =
-  k: injective_morphism K L k +
-  r: injective_morphism K R r 
-  for L K R k r
+  k: inclusion_morphism K L +
+  r: inclusion_morphism K R 
+  for L K R
+begin
+
+lemma k_subset_l:
+  \<open>V\<^bsub>K\<^esub> \<subseteq> V\<^bsub>L\<^esub>\<close> and \<open>E\<^bsub>K\<^esub> \<subseteq> E\<^bsub>L\<^esub>\<close>
+  by (simp_all add: subset_iff)
+
+lemma k_subset_r:
+  \<open>V\<^bsub>K\<^esub> \<subseteq> V\<^bsub>R\<^esub>\<close> and \<open>E\<^bsub>K\<^esub> \<subseteq> E\<^bsub>R\<^esub>\<close>
+  by auto  
+end
 
 notation rule ("_\<leftarrow>_\<rightarrow>_")
 
