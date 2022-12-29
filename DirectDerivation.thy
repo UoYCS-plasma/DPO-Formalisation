99a86a4c6772b07ec446acd1cd00da39fa63ec41
Robert Soeldner <r.soeldner@gmail.com>
2022-12-29 11:37:42 +0100

    Add additional lemmata and split direct_derivation and direct_derivation_construction

--- DirectDerivation.thy
+++ DirectDerivation.thy
@@ -2,22 +2,95 @@ theory DirectDerivation
   imports Rule Gluing Deletion
 begin
 
-locale direct_derivation =
-  r: rule L K R +
-  d: deletion K G L g idM +
-  g: gluing K d.D R g idM for G L K R g
 
+(* GRAT PDF P. 114 *)
+locale direct_derivation = 
+  r: rule r +
+ (*  m: injective_morphism "interf r" D m  +
+  *)
+  g: injective_morphism "lhs r" G g  +
+  po1: pushout_diagram "interf r" "lhs r" D G idM m g c +
+  po2: pushout_diagram "interf r" "rhs r" D H idM m f h
+  for r G g D m c H f h
 begin
 
-corollary 
-    \<open>pushout_diagram K L d.D G idM d.d g d.c'\<close> and \<open>pushout_diagram K R d.D g.H idM g g.h g.c\<close> 
+theorem
+  assumes 
+    po3: \<open>pushout_diagram (interf r) (lhs r) D' G idM m' g c'\<close> and
+    po4: \<open>pushout_diagram (interf r) (rhs r) D' H' idM m' f' h'\<close>
+  shows \<open>(\<exists>u. bijective_morphism D D' u) \<and> (\<exists>u. bijective_morphism H H' u)\<close>
+proof -
+  interpret po3: pushout_diagram "interf r" "lhs r" D' G idM m' g c'
+    using po3 by assumption
+
+  interpret m: injective_morphism "interf r" D m
+  proof
+    show \<open>inj_on \<^bsub>m\<^esub>\<^sub>V V\<^bsub>interf r\<^esub>\<close>
+      using po1.node_commutativity g.inj_nodes r.k.nodes_g_in_h
+      by (simp add: morph_comp_def inj_on_def)
+  next
+    show \<open>inj_on \<^bsub>m\<^esub>\<^sub>E E\<^bsub>interf r\<^esub>\<close>
+      using po1.edge_commutativity g.inj_edges r.k.edges_g_in_h
+      by (simp add: morph_comp_def inj_on_def)
+  qed
+     
+  interpret m': injective_morphism "interf r" D' m'
+  proof
+    show \<open>inj_on \<^bsub>m'\<^esub>\<^sub>V V\<^bsub>interf r\<^esub>\<close>
+      using po3.node_commutativity g.inj_nodes r.k.nodes_g_in_h
+      by (simp add: morph_comp_def inj_on_def)
+  next
+    show \<open>inj_on \<^bsub>m'\<^esub>\<^sub>E E\<^bsub>interf r\<^esub>\<close>
+      using po3.edge_commutativity g.inj_edges r.k.edges_g_in_h
+      by (simp add: morph_comp_def inj_on_def)
+  qed
+  thm po1.uniqueness_pc
+  have u:\<open>\<exists>u. bijective_morphism D D' u\<close>
+    using po1.uniqueness_pc[OF r.k.injective_morphism_axioms m.injective_morphism_axioms 
+                               po3.c.H.graph_axioms m'.injective_morphism_axioms
+                               po3.g.morphism_axioms] po3
+    by simp
+
+
+  (* rhs *)
+  interpret po4: pushout_diagram "interf r" "rhs r" D' H' idM m' f' h'
+    using po4 by assumption
+
+  obtain u where u:\<open>bijective_morphism D D' u\<close>
+    using u
+    by blast
+
+  interpret u: bijective_morphism D D' u
+    using u by assumption
+
+  have \<open>\<exists>u. bijective_morphism H H' u\<close>
+    by (meson bij_comp_bij_is_bij bijective_morphism.ex_inv po2.b_bij_imp_g_bij po4.b_bij_imp_g_bij r.r.bijective_morphism_axioms u)
+
+  show ?thesis
+    using \<open>\<exists>u. bijective_morphism H H' u\<close> u by blast
+    
+qed
+
+
+end
+
+locale direct_derivation_construction =
+  r: rule r +
+  d: deletion "interf r" G "lhs r" g idM +
+  g: gluing "interf r" d.D "rhs r" g idM for G r g H +
+assumes \<open>H = g.H\<close>
+begin
+
+corollary
+    \<open>pushout_diagram (interf r) (lhs r) d.D G idM d.d g d.c'\<close> and \<open>pushout_diagram (interf r) (rhs r) d.D g.H idM g g.h g.c\<close> 
   using 
     d.po.pushout_diagram_axioms
     g.po.pushout_diagram_axioms
   by simp_all
 
-end
 
-notation direct_derivation ("_ \<Rightarrow>\<^bsub>_ \<leftarrow> _ \<rightarrow> _, _\<^esub>")
+(* sublocale direct_derivation r G g d.D d.d d.c' g.H g.h g.c ..
+ *)
+end
 
 end
--- Rule.thy
+++ Rule.thy
@@ -2,21 +2,112 @@ theory Rule
   imports Morphism
 begin
 
+record ('v,'e,'l,'m) pre_rule =
+  lhs    :: "('v,'e,'l,'m) pre_graph"
+  interf :: "('v,'e,'l,'m) pre_graph"
+  rhs    :: "('v,'e,'l,'m) pre_graph"
+
 locale rule =
-  k: inclusion_morphism K L +
-  r: inclusion_morphism K R 
-  for L K R
+  k: inclusion_morphism "interf r" "lhs r" +
+  r: inclusion_morphism "interf r" "rhs r" 
+  for r :: "('v::countable,'e :: countable,'l,'m) pre_rule"
 begin
 
 lemma k_subset_l:
-  \<open>V\<^bsub>K\<^esub> \<subseteq> V\<^bsub>L\<^esub>\<close> and \<open>E\<^bsub>K\<^esub> \<subseteq> E\<^bsub>L\<^esub>\<close>
+  \<open>V\<^bsub>interf r\<^esub> \<subseteq> V\<^bsub>lhs r\<^esub>\<close> \<open>E\<^bsub>interf r\<^esub> \<subseteq> E\<^bsub>lhs r\<^esub>\<close>
   by (simp_all add: subset_iff)
 
 lemma k_subset_r:
-  \<open>V\<^bsub>K\<^esub> \<subseteq> V\<^bsub>R\<^esub>\<close> and \<open>E\<^bsub>K\<^esub> \<subseteq> E\<^bsub>R\<^esub>\<close>
+  \<open>V\<^bsub>interf r\<^esub> \<subseteq> V\<^bsub>rhs r\<^esub>\<close> \<open>E\<^bsub>interf r\<^esub> \<subseteq> E\<^bsub>rhs r\<^esub>\<close>
   by auto  
-end
 
-notation rule ("_\<leftarrow>_\<rightarrow>_")
+
+lemma lhs_morph_impl_interf:
+  assumes g: \<open>injective_morphism (lhs r) G g\<close>
+  shows \<open>injective_morphism (interf r) G g\<close>
+proof -
+  interpret g: injective_morphism \<open>lhs r\<close> G g
+    using g by assumption
+
+  show ?thesis
+  proof
+    show \<open>\<^bsub>g\<^esub>\<^sub>E e \<in> E\<^bsub>G\<^esub>\<close> if \<open>e \<in> E\<^bsub>interf r\<^esub>\<close> for e
+      using that 
+      by (simp add: g.morph_edge_range)
+  next
+    show \<open>\<^bsub>g\<^esub>\<^sub>V v \<in> V\<^bsub>G\<^esub>\<close> if \<open>v \<in> V\<^bsub>interf r\<^esub>\<close> for v
+      using that 
+      by (simp add: g.morph_node_range)
+  next
+    show \<open>\<^bsub>g\<^esub>\<^sub>V (s\<^bsub>interf r\<^esub> e) = s\<^bsub>G\<^esub> (\<^bsub>g\<^esub>\<^sub>E e)\<close> if \<open>e \<in> E\<^bsub>interf r\<^esub>\<close> for e
+      using that g.source_preserve k.source_preserve
+      by simp
+  next
+    show \<open>\<^bsub>g\<^esub>\<^sub>V (t\<^bsub>interf r\<^esub> e) = t\<^bsub>G\<^esub> (\<^bsub>g\<^esub>\<^sub>E e)\<close> if \<open>e \<in> E\<^bsub>interf r\<^esub>\<close> for e
+      using that g.target_preserve k.target_preserve
+      by simp
+  next
+    show \<open>l\<^bsub>interf r\<^esub> v = l\<^bsub>G\<^esub> (\<^bsub>g\<^esub>\<^sub>V v)\<close> if \<open>v \<in> V\<^bsub>interf r\<^esub>\<close> for v
+      using that
+      by (simp add: g.label_preserve k.label_preserve)
+  next
+    show \<open>m\<^bsub>interf r\<^esub> e = m\<^bsub>G\<^esub> (\<^bsub>g\<^esub>\<^sub>E e)\<close> if \<open>e \<in> E\<^bsub>interf r\<^esub>\<close> for e
+      using that
+      by (simp add: g.mark_preserve k.mark_preserve)
+  next
+    show \<open>inj_on \<^bsub>g\<^esub>\<^sub>V V\<^bsub>interf r\<^esub>\<close>
+      using inj_on_subset[OF g.inj_nodes k_subset_l(1)]
+      by assumption
+  next
+    show \<open>inj_on \<^bsub>g\<^esub>\<^sub>E E\<^bsub>interf r\<^esub>\<close>
+      using inj_on_subset[OF g.inj_edges k_subset_l(2)]
+      by assumption
+  qed
+qed
+
+lemma rhs_morph_impl_interf:
+  assumes g: \<open>injective_morphism (rhs r) G g\<close>
+  shows \<open>injective_morphism (interf r) G g\<close>
+proof -
+  interpret g: injective_morphism \<open>rhs r\<close> G g
+    using g by assumption
+
+  show ?thesis
+  proof
+    show \<open>\<^bsub>g\<^esub>\<^sub>E e \<in> E\<^bsub>G\<^esub>\<close> if \<open>e \<in> E\<^bsub>interf r\<^esub>\<close> for e
+      using that g.morph_edge_range
+      by blast
+  next
+    show \<open>\<^bsub>g\<^esub>\<^sub>V v \<in> V\<^bsub>G\<^esub>\<close> if \<open>v \<in> V\<^bsub>interf r\<^esub>\<close> for v
+      using that g.morph_node_range
+      by blast
+  next
+    show \<open>\<^bsub>g\<^esub>\<^sub>V (s\<^bsub>interf r\<^esub> e) = s\<^bsub>G\<^esub> (\<^bsub>g\<^esub>\<^sub>E e)\<close> if \<open>e \<in> E\<^bsub>interf r\<^esub>\<close> for e
+      using that g.source_preserve k.source_preserve
+      using r.edges_g_in_h r.source_preserve by force
+  next
+    show \<open>\<^bsub>g\<^esub>\<^sub>V (t\<^bsub>interf r\<^esub> e) = t\<^bsub>G\<^esub> (\<^bsub>g\<^esub>\<^sub>E e)\<close> if \<open>e \<in> E\<^bsub>interf r\<^esub>\<close> for e
+      using that g.target_preserve k.target_preserve
+      using r.edges_g_in_h r.target_preserve by auto
+  next
+    show \<open>l\<^bsub>interf r\<^esub> v = l\<^bsub>G\<^esub> (\<^bsub>g\<^esub>\<^sub>V v)\<close> if \<open>v \<in> V\<^bsub>interf r\<^esub>\<close> for v
+      using that g.label_preserve k.label_preserve r.label_preserve
+      using r.nodes_g_in_h by auto
+  next
+    show \<open>m\<^bsub>interf r\<^esub> e = m\<^bsub>G\<^esub> (\<^bsub>g\<^esub>\<^sub>E e)\<close> if \<open>e \<in> E\<^bsub>interf r\<^esub>\<close> for e
+      using that g.mark_preserve k.mark_preserve
+      using r.edges_g_in_h r.mark_preserve by auto
+  next
+    show \<open>inj_on \<^bsub>g\<^esub>\<^sub>V V\<^bsub>interf r\<^esub>\<close>
+      using inj_on_subset[OF g.inj_nodes k_subset_r(1)]
+      by assumption
+  next
+    show \<open>inj_on \<^bsub>g\<^esub>\<^sub>E E\<^bsub>interf r\<^esub>\<close>
+      using inj_on_subset[OF g.inj_edges k_subset_r(2)]
+      by assumption
+  qed
+qed
+
+end
 
 end
